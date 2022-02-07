c438a4b2b9d01743b302db06fec42c092d94b1bb
alinaot <alinaot20@gmail.com>
2022-02-08 01:15:01 +0700

    added pagination

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -22,13 +22,18 @@ namespace WpfApp1
     public partial class MainWindow : Window
     {
         ClientServiceEntities context;
+        public int countPages;
+        public int currentPage;
+        public int countRecord = 10;
+
         public MainWindow()
         {
             InitializeComponent();
             context = new ClientServiceEntities();
-            dgClients.ItemsSource = context.Clients.ToList();
-
-            
+            dgClients.ItemsSource = context.Clients.Take(countRecord).ToList();
+            countPages = context.Clients.Count() / 10;
+            currentPage = 1;
+            CurrentPage.Text = currentPage + " из " + countPages;
         }
 
         private void AddData_Click(object sender, RoutedEventArgs e)
@@ -41,22 +46,88 @@ namespace WpfApp1
         {
 
         }
+        private void ShowTable()
+        {
+            dgClients.ItemsSource = context.Clients.Take(countRecord).ToList();
+        }
 
         private void cmbSelectCount_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            currentPage = 1;
+            ClientServiceEntities context2 = new ClientServiceEntities();
 
+            switch (cmbSelectCount.SelectedIndex)
+            {
+                case 0:
+
+                    countRecord = 10;
+                    countPages = context2.Clients.Count() / countRecord;
+                    break;
+                case 1:
+                    countRecord = 50;
+                    countPages = context2.Clients.Count() / countRecord;
+
+                    break;
+                case 2:
+                   
+                    countRecord = 200;
+                    countPages = context2.Clients.Count() / countRecord;
+                    if (countPages == 0)
+                        countPages = 1;
+                    break;
+                default:
+                    countRecord = context2.Clients.Count();
+                    countPages = 1;
+                    break;
+            } 
         }
 
         private void PrevPage_Click(object sender, RoutedEventArgs e)
         {
+            
+            if (currentPage > 1 && currentPage <= countPages)
+            {
+                currentPage--;
+                dgClients.ItemsSource = context.Clients.OrderBy(x => x.ID).Skip(currentPage * countRecord-countRecord).Take(countRecord).ToList();
+                CurrentPage.Text = currentPage + " из " + countPages;
+                NextPage.IsEnabled = true;
+                if(currentPage == 1)
+                {
+                    PrevPage.IsEnabled = false;
+                }
 
+            } 
         }
 
         private void NextPage_Click(object sender, RoutedEventArgs e)
         {
-
+            if (currentPage < countPages)
+            {
+                dgClients.ItemsSource = context.Clients.OrderBy(x=>x.ID).Skip(currentPage*countRecord).Take(countRecord).ToList();
+                currentPage++;
+                CurrentPage.Text = currentPage + " из " + countPages;
+                PrevPage.IsEnabled = true;
+                if (currentPage == countPages)
+                {
+                    NextPage.IsEnabled = false;
+                }
+            }
+            
         }
- 
-       
+
+        private void showRecord_Click(object sender, RoutedEventArgs e)
+        {
+            if (countPages == 1)
+            {
+                PrevPage.IsEnabled = false;
+                NextPage.IsEnabled = false;
+            } else
+            {
+                PrevPage.IsEnabled = true;
+                NextPage.IsEnabled = true;
+            }
+            CurrentPage.Text = currentPage + " из " + countPages;
+            ShowTable();
+        }
     }
 }
